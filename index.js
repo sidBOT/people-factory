b5ad74072b8818d81e9ed4502c1e27a384120a74
Siddhant Patel <si12hant@gmail.com>
2017-06-05 12:13:14 -0400

    update heading with text box value

---
+++ index.js
@@ -0,0 +1,8 @@
+const personForm = document.querySelector('#personForm')
+function handleSubmit(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const name = f.personName.value
+    document.querySelector('h1').textContent = name;
+}
+personForm.addEventListener('submit', handleSubmit)
