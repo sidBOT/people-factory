f84e71f22387d5ff77e12987b70e7896b1162965
Siddhant Patel <si12hant@gmail.com>
2017-06-06 10:20:13 -0400

    display fav color

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ function handleSubmit(ev) {
     const pass = f.password.value
     const details = document.querySelector('#details')
     var string = "name: " + name + " pass: " + pass;
+    const favouriteColor = f.favouriteColor.value;
+    const colorDiv = `<div style="background-color: ${favouriteColor}; width: 100px;height: 50px"></div>
+    `
     document.querySelector('h1').textContent = string;
     //details.innerHTML = '<em>' + name + '</em>'
     //details.innerHTML =`<em>${name}</em>`;
     // const em = document.createElement('em');
     // em.textContent = name;
     // details.appendChild(em);
-
-    const favouriteColor = f.favouriteColor.value;
     const age = f.age.value;
     details.innerHTML = `
         <ul>
             <li>Name: ${name}</li>
-            <li>Favourite color: ${favouriteColor}</li>
+            <li>Favourite color: ${colorDiv}</li>
             <li>Age: ${age}</li>
 
         </ul>
